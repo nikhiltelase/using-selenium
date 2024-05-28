4dce14e914b2c8c8c19c23b45eeb038d9a891d71
nikhil <nikhiltelase@gmail.com>
2024-05-28 10:48:34 +0530

    some updats

--- linkedin_bot.py
+++ linkedin_bot.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-EMAIL = "Eamil"
-PASSWORD = "Your password"
+EMAIL = "nikhiltelase@gmail.com"
+PASSWORD = "Pintu123@"
 LINK = "https://www.linkedin.com/feed/"
 
 
